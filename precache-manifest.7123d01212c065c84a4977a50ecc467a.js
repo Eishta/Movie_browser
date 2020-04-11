253db350b3b99e398af12d283793c14b26bc0b75
Eishta <eishtamittal@gmail.com>
2020-04-11 17:04:26 +0530

    Updates

---
+++ precache-manifest.7123d01212c065c84a4977a50ecc467a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ad0692a6db41fd3a4b26442c81b28c",
+    "url": "/GSIV20_Eishta_Mittal/index.html"
+  },
+  {
+    "revision": "d96d15199d0703a4df7c",
+    "url": "/GSIV20_Eishta_Mittal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6ef1bc1f50193bd1c68e",
+    "url": "/GSIV20_Eishta_Mittal/static/js/2.70a997b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GSIV20_Eishta_Mittal/static/js/2.70a997b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96d15199d0703a4df7c",
+    "url": "/GSIV20_Eishta_Mittal/static/js/main.c72fda83.chunk.js"
+  },
+  {
+    "revision": "5fcb6eda9e35273867a5",
+    "url": "/GSIV20_Eishta_Mittal/static/js/runtime-main.226a03e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GSIV20_Eishta_Mittal/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/GSIV20_Eishta_Mittal/precache-manifest.7123d01212c065c84a4977a50ecc467a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GSIV20_Eishta_Mittal/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
